5323347d823914c8933b3f49a2e3c2ff4e703da2
Vitaliy Kibish <vt@Vitaliys-MacBook-Pro.local>
2020-05-25 09:37:49 +0200

    Updates

---
+++ precache-manifest.85f4ff4c3379d88aadef2885f425f574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a596ff71993e5899e334c180eb6b81",
+    "url": "/travel-app/index.html"
+  },
+  {
+    "revision": "d739ecc0aa66cb417634",
+    "url": "/travel-app/static/css/main.e426d0a8.chunk.css"
+  },
+  {
+    "revision": "bdf25ca00126ff17d1b4",
+    "url": "/travel-app/static/js/2.6d5bda8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travel-app/static/js/2.6d5bda8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d739ecc0aa66cb417634",
+    "url": "/travel-app/static/js/main.6519d995.chunk.js"
+  },
+  {
+    "revision": "6c1e563da327d5330d00",
+    "url": "/travel-app/static/js/runtime-main.f8619c4f.js"
+  }
+]);
